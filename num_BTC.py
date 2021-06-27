0d17913a5bc496c6fae1db7063445a63382cd341
SelphieDou <57315805+SelphieDou@users.noreply.github.com>
2021-06-27 16:30:37 +0800

    v1

---
+++ num_BTC.py
@@ -0,0 +1,17 @@
+import math
+
+def num_BTC(b):
+	circulate = 50
+	round = b // 210000
+	print(round)
+	extra = b % 210000
+	total = 0
+	while round > 0:
+		total += 210000 * circulate
+		round = round - 1
+		circulate = circulate / 2
+	total += circulate * extra
+    c = float(total)
+    return c
+
+
